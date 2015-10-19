ce9c87d53d77212e6e8ecd2cc284af5e9dfe308f
Juanlu <juanlu.delafuente7@gmail.com>
2015-10-19 11:14:16 +0200

    AML-showcase v0.1, 2015-10-19 ----------------------- - Refactor Papamoscas showcase #1 - Add copyright header #2 - Review md #3
    Fixes #1, #2, #3

---
+++ README.md
@@ -0,0 +1,17 @@
+# Agreement Management Library Showcases
+
+In this project, you will find some examples illustrating the usage of Agreement Management Library. For more information about AML, please visit [this site]("http://isa-group.github.io/aml/").
+
+## Requirements
+
+Mandatory requirements:
+* [Java SE Development Kit 7 or newer](http://www.oracle.com/technetwork/es/java/javase/downloads/index.html)
+
+Recommended: 
+* [Eclipse IDE for Java EE Developers](http://www.eclipse.org/downloads/packages/eclipse-ide-java-ee-developers/marsr)
+
+***
+## Contributing
+
+To contribute to this project, read our [development policies](https://github.com/isa-group/aml/wiki#development-policy)
+***
---
+++ papamoscas/src/main/java/es/us/isa/papamoscas/entities/AbstractFacade.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * AML is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AML is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AML. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) ISA Research Group - University of Sevilla, 2015
+ * Licensed under GPL (https://github.com/isa-group/aml/blob/master/LICENSE.txt)
+ *******************************************************************************/
+package es.us.isa.papamoscas.entities;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+/**
+ *
+ * @author AntonioGamez
+ */
+public abstract class AbstractFacade<T> {
+
+    private Class<T> entityClass;
+
+    public AbstractFacade(Class<T> entityClass) {
+        this.entityClass = entityClass;
+    }
+
+    protected abstract EntityManager getEntityManager();
+
+    public void create(T entity) {
+        getEntityManager().persist(entity);
+    }
+
+    public void edit(T entity) {
+        getEntityManager().merge(entity);
+    }
+
+    public void remove(T entity) {
+        getEntityManager().remove(getEntityManager().merge(entity));
+    }
+
+    public T find(Object id) {
+        return getEntityManager().find(entityClass, id);
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+	public List<T> findAll() {
+        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entityClass));
+        return getEntityManager().createQuery(cq).getResultList();
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public List<T> findRange(int[] range) {
+        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entityClass));
+        javax.persistence.Query q = getEntityManager().createQuery(cq);
+        q.setMaxResults(range[1] - range[0] + 1);
+        q.setFirstResult(range[0]);
+        return q.getResultList();
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public int count() {
+        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        javax.persistence.criteria.Root<T> rt = cq.from(entityClass);
+        cq.select(getEntityManager().getCriteriaBuilder().count(rt));
+        javax.persistence.Query q = getEntityManager().createQuery(cq);
+        return ((Long) q.getSingleResult()).intValue();
+    }
+
+}
