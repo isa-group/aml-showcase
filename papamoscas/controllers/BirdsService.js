8a41258ffc237f33aa539b63f257363ed54751e9
Antonio Gámez Díaz <antgamdia@gmail.com>
2016-02-02 15:05:47 +0100

    Simple API using Nedb

--- papamoscas/controllers/BirdsService.js
+++ papamoscas/controllers/BirdsService.js
@@ -1,108 +1,87 @@
 'use strict';
-var examples = {};
-  
-  examples['application/json'] = [ {
-  "specie" : "aeiou",
-  "eggs" : 1.3579000000000001069366817318950779736042022705078125,
-  "hatches" : 1.3579000000000001069366817318950779736042022705078125,
-  "legDiameter" : 1.3579000000000001069366817318950779736042022705078125,
-  "id" : 123456789,
-  "place" : "aeiou",
-  "wingSize" : 1.3579000000000001069366817318950779736042022705078125
-} ];
+var path = require('path');
+var DataStore = require('nedb');
+var dbFileName = path.join(__dirname, 'birds.json');
+var db = new DataStore({
+    filename: dbFileName,
+    autoload: true
+});
+
+db.find({}, function(err, birds) {
+
+    if (birds.length == 0) {
+        console.log('Empty DB, loading initial data');
+
+       var bird1 = {
+            specie: 'Ficedula hypoleuca iberiae',
+            eggs: '30',
+            hatches: '12',
+            legDiameter: '13.3',
+            id: '1',
+            place: 'Huelva',
+            wingSize: '20.1'
+        };
+
+        db.insert([bird1]);
+
+    } else {
+        console.log('DB has ' + birds.length + ' birds ');
+    }
+
+});
 
 exports.birdsGet = function(args, res, next) {
-  /**
-   * parameters expected in the args:
-   
 
-var examples = {};
-  
-  examples['application/json'] = [ {
-  "specie" : "aeiou",
-  "eggs" : 1.3579000000000001069366817318950779736042022705078125,
-  "hatches" : 1.3579000000000001069366817318950779736042022705078125,
-  "legDiameter" : 1.3579000000000001069366817318950779736042022705078125,
-  "id" : 123456789,
-  "place" : "aeiou",
-  "wingSize" : 1.3579000000000001069366817318950779736042022705078125
-} ];
-  **/
+    console.log('New GET request');
 
-  
-  if(Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  }
-  else {
-    res.end();
-  }
-  
-  
+    db.find({}, function(err, birds) {
+            if (birds.length > 0) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(birds));
+        } else {
+            res.status(404);
+			res.header("Content-Type", "application/json; charset=utf-8");
+            res.end();
+        }
+    });
 }
+	
 exports.birdsPut = function(args, res, next) {
-  /**
-   * parameters expected in the args:
-   * bird (Bird)
-   **/
-
-var examples = {};
-  
-
-  
-  res.end();
-}
-exports.birdsPost = function(args, res, next) {
-  /**
-   * parameters expected in the args:
-   * bird (Bird)
-   **/
-
-  examples['application/json'].push(args.bird.value);
-  
-
-  
-  res.end();
-}
-exports.birdsDelete = function(args, res, next) {
-  /**
-   * parameters expected in the args:
-   * bird (Bird)
-   **/
-
-var examples = {};
-  
-
-  
-  res.end();
-}
-exports.birdsIdGet = function(args, res, next) {
-  /**
-   * parameters expected in the args:
-   * id (BigDecimal)
-   **/
-
-var examples = {};
-  
-  examples['application/json'] = {
-  "specie" : "aeiou",
-  "eggs" : 1.3579000000000001069366817318950779736042022705078125,
-  "hatches" : 1.3579000000000001069366817318950779736042022705078125,
-  "legDiameter" : 1.3579000000000001069366817318950779736042022705078125,
-  "id" : 123456789,
-  "place" : "aeiou",
-  "wingSize" : 1.3579000000000001069366817318950779736042022705078125
-};
-  
-
-  
-  if(Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  }
-  else {
+    console.log('New PUT request');
+    res.sendStatus(501);
     res.end();
-  }
-  
-  
 }
+
+/*
+exports.birdsPost = function(args, res, next) {
+    console.log('New POST request');
+    console.log(args.bird.value));
+db.insert(args);
+res.end();
+}
+/*
+exports.birdsDelete = function(args, res, next) {
+    console.log('New DELETE request');
+    res.sendStatus(501);
+    res.end();
+    res.end();
+}
+
+exports.birdsIdGet = function(args, res, next) {
+
+
+    var id = args.id.value;
+    console.log('New DELETE request for bird with id ' + id);
+
+    db.remove({
+        _id: id
+    }, {}, function(err, numRemoved) {
+        console.log("Birds removed: " + numRemoved);
+        if (numRemoved == 1)
+            res.sendStatus(200);
+        else
+            res.sendStatus(404);
+    });
+
+
+}*/
